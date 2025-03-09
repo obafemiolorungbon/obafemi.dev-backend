a3efc481aa997df639c447bae6eb482d2570f356
obafemiolorungbon <obafedance@gmail.com>
2025-03-09 21:43:22 +0100

    fix: fixed port issue for the deployment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsConfig } from './config/cors.config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,9 +10,12 @@ async function bootstrap() {
   // Starts listening for shutdown hooks
   app.enableShutdownHooks();
 
+  const configService = app.get(ConfigService);
+  const port = configService.get('app.port');
+
   // Set a global prefix for all routes
   app.setGlobalPrefix('api/v1');
 
-  await app.listen(4300);
+  await app.listen(port);
 }
 bootstrap().catch((err) => console.error('Error starting server:', err));
